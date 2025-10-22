56b00e2461d10475b54452445b92ea161bbc4961
PalaeMal <palaemal@gmail.com>
2025-10-22 18:05:11 +0630

    test commit

--- Model/DTO/AddInventoryTransactionDTO.cs
+++ Model/DTO/AddInventoryTransactionDTO.cs
@@ -7,14 +7,14 @@ using System.Threading.Tasks;
 
 namespace Model.DTO
 {
-    //public class AddInventoryTransactionDTO
-    //{
-    //    public Guid productId { get; set; }
-    //    public int qtyChange { get; set; }
-    //    public string? transactionType { get; set; }
-    //    public Guid referenceId { get; set; }
-    //    public bool? activeFlag { get; set; }
-    //    public string? createdBy { get; set; }
-    //    public DateTime createdDate { get; set; } = DateTime.UtcNow;
-    //}
+    public class AddInventoryTransactionDTO
+    {
+        public Guid productId { get; set; }
+        public int qtyChange { get; set; }
+        public string? transactionType { get; set; }
+        public Guid referenceId { get; set; }
+        public bool? activeFlag { get; set; }
+        public string? createdBy { get; set; }
+        public DateTime createdDate { get; set; } = DateTime.UtcNow;
+    }
 }
